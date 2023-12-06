1e6d5f4d7100c1dbb2673c875c1485e0b83f0c8c
Ben Arthur <benarthur@live.com>
2023-12-06 19:13:41 +1100

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// https://BenArthurOCE.github.io/TaxRatesJSON/atoJSON.json.
+
+// https://benarthuroce.github.io/TaxRatesJSON/atoJSON.json
+
+
+//https://github.com/BenArthurOce/TaxRatesJSON
+
+//https://github.com/BenArthurOce/TaxRatesJSON/blob/main/atoJSON.json
